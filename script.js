d8d7de18d6ba83bd98067d7cedf86a92fcf537df
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-04-14 23:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
 
-const person = {
-  name: "John Doe",
-  age: 80,
-};
+ 
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+let team = players;
+let team1 = [];
+for(let i in players){
+	team1.push(players[i]);
+}
+
+class A{
+	constructor(person){
+		this.name = person.name;
+		this.age = person.age;
+	}
+}
+let cap1 = constructor(person);
+
 
 window.players = players;
 window.person = person;
